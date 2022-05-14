c6d0789eac0cface882583c207f8e886852195df
Newton Lomar <newtonlomar16@gmail.com>
2022-05-14 14:36:03 -0300

    React state hook counter example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
